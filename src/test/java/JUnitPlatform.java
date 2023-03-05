573a956c93f83fba7b55a382cb867fb1a059f9d1
Roman Bragin <bragin-9696@bk.ru>
2023-03-05 21:49:57 +0500

    added .gitignore and build.gradle

---
+++ src/test/java/JUnitPlatform.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+class JUnitPlatform {
+
+    @Test
+    void codeSampleCheckTest() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".js-wiki-more-pages-link").click();
+        $(".wiki-rightbar").shouldHave(text("JUnitPlatform"));
+        $(".wiki-rightbar").$(byText("JUnitPlatform")).click();
+        $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+
+        sleep(3000);
+
+    }
+}
+
+
+
