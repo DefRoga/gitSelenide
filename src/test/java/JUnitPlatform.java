f96784092d5b6d8d321b207cc9ac9bf359735215
Roman Bragin <bragin-9696@bk.ru>
2023-03-05 21:55:25 +0500

    renamed class and fixed some problems

--- src/test/java/JUnitPlatform.java
+++ src/test/java/JUnitPlatform.java
@@ -4,19 +4,17 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
-class JUnitPlatform {
+class CodeForFindingJunit5 {
 
     @Test
     void codeSampleCheckTest() {
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $(".js-wiki-more-pages-link").click();
-        $(".wiki-rightbar").shouldHave(text("JUnitPlatform"));
-        $(".wiki-rightbar").$(byText("JUnitPlatform")).click();
+        $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
+        $(".wiki-rightbar").$(byText("SoftAssertions")).click();
+        //$("#user-content-3-using-junit5-extend-test-class а").$((byText("5. Using JUnit5 extend test class:")));
         $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
-
     }
 }
-
-
 
