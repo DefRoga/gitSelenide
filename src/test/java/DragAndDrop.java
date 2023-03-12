29103fee174f519a93e44cf110de3f712f39b685
Roman Bragin <bragin-9696@bk.ru>
2023-03-12 20:44:53 +0500

    done first commit

---
+++ src/test/java/DragAndDrop.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDrop {
+    @Test
+    void ChangingPlaces () {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#columns div").shouldHave(text("B"));
+        sleep(4000);
+
+    }
+}
